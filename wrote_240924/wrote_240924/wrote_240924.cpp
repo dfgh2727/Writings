b1a301d9107e985562344208bec98c253dd0ab17
dfgh2727 <dfgh2727@naver.com>
2024-10-07 10:57:32 +0900

    이수환: 쿠리시구레 살살 녹는다

--- wrote_240924/wrote_240924/wrote_240924.cpp
+++ wrote_240924/wrote_240924/wrote_240924.cpp
@@ -1,5 +1,15 @@
-﻿ooo
-ooooo
-oooo
-ooooo
-oooo
+﻿#include <iostream>
+
+
+{
+	int Value = 0; // 메모리 영역을 만든다
+	// 가장 오른쪽 메모리 영역으로만 판단한다.
+	while (Value += 1, Value != 10)
+	{
+	}
+}
+
+void main()
+{
+
+}
