6e4f89783d4d98acdce728c41a3c217ff48fd60b
dfgh2727 <dfgh2727@naver.com>
2024-10-14 15:56:33 +0900

    이수환: new를 선언할 때 조심해...

---
+++ wrote_240924/ggggg/ggggg.cpp
@@ -0,0 +1,86 @@
+﻿// 074_VirtualInstanceEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class AActor
+{
+public:
+    virtual ~AActor()
+    {
+
+    }
+};
+
+// Monster Is A AActor
+// Player* => AActor* => UpCasting
+class Monster : public AActor
+{
+};
+
+// Player Is A AActor
+// Player* => AActor* => UpCasting
+class Player : public AActor
+{
+};
+
+// 게임에서 나올 모든 눈에 보이는 오브젝트가 여기안에 담겨있을 것이다.
+// 게임마다 달라. 예상할수 있다고
+// 유저는 항상 개발의 상상력을 깹니다. 상식적으로 유저를 접근하면 안됩니다.
+// 그래서 항상 가장 다형적인 측면으로 접근해도 된다.
+// 일단 문제가 없게 접근해야 합니다.
+// 일단 상수가 절대로 가능하지가 않아요.
+
+// 최초에 몇개를 만들지 정하지 않아요. 일반적으로
+
+AActor** AllActorList = nullptr;
+int AllActorSize = 0;
+
+// 이런식의 함수를 만듭니다.
+void SpawnActor(AActor* _Actor)
+{
+    // 새로만든 플레이어를 받을공간이 없으므로
+
+    AllActorList = new AActor * [AllActorSize];
+
+    AllActorList[AllActorSize] = _Actor;
+}
+
+
+int main()
+{
+    // 그러면서도 leck은 없어야 합니다.
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    //// 10마리 10마리를 만든다음 다음과 같이 해야한다.
+    //Player NewPlayer[10];
+    //Monster NewMonster[10];
+
+    //// 당연히 됩니다.
+    //// 참조형이기 때문에 아주 간단
+    //Player* Ptr = new Player[10];
+    //Monster* Ptr0 = new Monster[10];
+
+    // 플레이어를 잃어버린것이다.
+    // AActor* Ptr = new Player();
+
+    SpawnActor(new Player());
+    SpawnActor(new Player());
+
+    // 왜터지는지 설명할수 있나요
+
+
+    for (size_t i = 0; i < AllActorSize; i++)
+    {
+        delete AllActorList[i];
+    }
+
+
+    delete[] AllActorList;
+
+    //SpawnActor(new Player());
+    //SpawnActor(new Player());
+    //SpawnActor(new Player());
+
+
+}
--- wrote_240924/wrote_241014/wrote_241014.cpp
+++ wrote_240924/wrote_241014/wrote_241014.cpp
@@ -1,12 +1,6 @@
 ﻿// wrote_241014.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <iostream>
-
-int main()
-{
-    
-}
 
 //F10, 프로그램 시작 위치로 간다.
 
@@ -32,11 +26,105 @@ int main()
 
 //갤러그와 다형성(?)
 // 
-//먼저 클래스를 만들어 보면...
-//플레이어, 몬스터, bullet 이렇게 3가지를 만들었다.
-//(클래스의 종류가 많을수록 관리가 어려워진다. 그래서 업캐스팅으로 만들어야 한다.)
+//플레이어, 몬스터, bullet 이 필요한데...
+//(클래스의 종류가 많을수록 관리가 어려워진다. 따로 만들기보다 다형성으로 만들어야 한다.)
 // 
-// 몬스터와 불렛을 동적으로 
+// 몬스터, 플레이어, 불렛 모두 공통점? 위치가 있다 -> FintPoint
+// ...AActor를 Super로 typedef.
+// 
+// 
+// 몬스터와 불렛 수가 정해져 있나? 아니라면 동적할당
+// AActor** AllActorList = new AActor* ();
+// 포인터를 담을 수 있는 포인터의 집합... Player인지 Monster인지 상관없다.
+// 게임에 나올 오브젝트가 다 여기에 담겨 있는데...수를 예상할 수 없다.
+// AActor** AllActorList = new AActor*[100]; (X)  
+//                                    이렇게 상수로? 안된다. 일단 문제가 없게 수를 정하지 말자.
+// (new Player(); 이렇게 하면 메모리 잃어버린거다;; 항상 주소 받아야 쓴다)
+// new를 선언했다 -> 힙에 메모리 영역 생김
+// 
+ 
+
+//074_
+#include <iostream>
+
+class AActor
+{
+public:
+    virtual ~AActor()
+    {
+
+    }
+};
+
+// Monster Is A AActor
+// Player* => AActor* => UpCasting
+class Monster : public AActor
+{
+};
+
+// Player Is A AActor
+// Player* => AActor* => UpCasting
+class Player : public AActor
+{
+};
+
+AActor** AllActorList = nullptr;
+int AllActorSize = 0;
+
+// 이런식의 함수를 만듭니다.
+void SpawnActor(AActor* _Actor)
+{
+    // 새로만든 플레이어를 받을공간이 없으므로
+
+    AllActorList = new AActor*[AllActorSize + 1];
+
+    AllActorList[AllActorSize] = _Actor;
+    ++AllActorSize;
+}
+
+
+int main()
+{
+    // 그러면서도 leck은 없어야 합니다.
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+    SpawnActor(new Player());
+    //AllActorList = new AActor * [1];
+    //AllActorList[AllActorSize] = _Actor;
+    //++AllActorSize;
+
+    SpawnActor(new Player());
+    //AllActorList = new AActor * [2];
+    //AllActorList[AllActorSize] = _Actor;
+    //++AllActorSize;
+    
+    // 터지는 이유 (그림 꼭 그려보기):
+    // AllActorList = new AActor * [1];
+    // AllActorList[AllActorSize] = _Actor;
+    // ++AllActorSize; 
+    // 
+    // AllActorList = new AActor * [2]; 를 선언, 2칸짜리 배열 생성.
+    // 이때 AActor[1]을 가리키던 AllActorList는 2칸 배열을 가리키면서 한칸짜리 배열을 위치를 잃어버림. (leak 발생)
+    // 두칸짜리의 [1]에 ptr를 선언해줬다. [0]의 값은 알 수 없다. 근데 [0]의 값을 delete시도. 터진다.
+
+    for (size_t i = 0; i < AllActorSize; i++)
+    {
+        delete AllActorList[i];
+    }
+
+
+    delete[] AllActorList;
+
+}
+//생각한대로 두번 생기고 한번만 삭제되는건 맞지만 
+//구체적으로 터진 이유는 위치를 모르는 메모리를 제거하려 했기 때문이다.
+//두칸짜리 배열의 생성을 생각했어야 했다. 
+
 //
+//
+//
+// 
+
 
 
