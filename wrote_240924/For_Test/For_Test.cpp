4c4db90754c784847de2ab0f5b0b3880457f9e47
dfgh2727 <dfgh2727@naver.com>
2024-10-25 15:42:27 +0900

    이수환: (API) EngineTimer 함수로 시간 재기

---
+++ wrote_240924/For_Test/For_Test.cpp
@@ -0,0 +1,19 @@
+﻿// For_Test.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int A = 0;
+
+	for (unsigned char i = 0; i < 300; i++)
+	{
+		++A;
+	}
+
+	print_f("%c", A);
+
+	return 0;
+}
+
