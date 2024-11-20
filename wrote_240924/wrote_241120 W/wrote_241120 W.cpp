8cc34e782325550fb012d0d3a21eba9565332d4e
dfgh2727 <dfgh2727@naver.com>
2024-11-20 10:59:44 +0900

    이수환:

---
+++ wrote_240924/wrote_241120 W/wrote_241120 W.cpp
@@ -0,0 +1,18 @@
+﻿// wrote_241120 W.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// (SpriteRenderer.cpp)
+// HPbar 만들다가 Pivot 만지기...;;;
+// 
+// Pivot {0.5, 0.5} 은 center ({1, 1}을 전체 크기로 생각)
+// 
+// 
+// SetPivotValue()를 통해 직접 Pivot값을 정해줄 수도 있다.
+
