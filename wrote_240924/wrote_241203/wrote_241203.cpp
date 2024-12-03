4e41bb64715abd55d00b12acebfd8a2847df4a8d
dfgh2727 <dfgh2727@naver.com>
2024-12-03 12:16:40 +0900

    이수환:

---
+++ wrote_240924/wrote_241203/wrote_241203.cpp
@@ -0,0 +1,17 @@
+﻿// wrote_241203.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// ranged for
+// 순회를 돌린다(iterator... begin(), end())
+// 즉, 절대로 삭제하면 안 된다. (erase() 쓰지 말기)
+// 
+// 
+// 
+// 
