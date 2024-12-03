b824e6dcf90ee9e536b4076113dd041635547230
dfgh2727 <dfgh2727@naver.com>
2024-12-03 17:00:13 +0900

    이수환: ranged for, region

--- wrote_240924/wrote_241203/wrote_241203.cpp
+++ wrote_240924/wrote_241203/wrote_241203.cpp
@@ -3,15 +3,23 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
 // ranged for
 // 순회를 돌린다(iterator... begin(), end())
 // 즉, 절대로 삭제하면 안 된다. (erase() 쓰지 말기)
+
+// region 
+// 묶어준다.
 // 
-// 
-// 
-// 
+
+int main()
+{
+
+#pragma region Main
+    std::cout << "Hello World!\n";
+#pragma endregion comment...
+
+}
+
+// 오늘 한 것...
+//
+// 도형 만들기, 회전 시키기
