6933f35889bb80415fde5326b1bea666ec140eaa
dfgh2727 <dfgh2727@naver.com>
2024-12-09 16:10:39 +0900

    이수환:

---
+++ wrote_240924/wrote_241209 M/wrote_241209 M.cpp
@@ -0,0 +1,19 @@
+﻿// wrote_241209 M.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 오늘 한 것
+// 
+// Identity(항등행렬)
+// 4x4 행렬을 이용한 FVector (x, y, z, w)의 크기변환, 이동
+// 회전변환 (x축, y축, z축) 
+// 
+// 
+// 
+
