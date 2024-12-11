a3bb881afe8301b91ea0c324103181087d5ee38c
dfgh2727 <dfgh2727@naver.com>
2024-12-11 17:34:47 +0900

    이수환: DX용 솔루션 파일 만들기

--- wrote_240924/wrote_241211/wrote_241211.cpp
+++ wrote_240924/wrote_241211/wrote_241211.cpp
@@ -62,6 +62,19 @@ void PerspectiveFov(float _FovAngle, float _Width, float Height, float _Near, fl
 // 나중에 w를 1.0f로 바꾼다.
 // https://openmynotepad.tistory.com/102
 // 
-//  
+
+
+
+
+
+// DX 시작
 // 
+// 출력 디렉토리 API것을 복붙하고 Build를 쓰고 중간(Build/Mid)과 실행파일(Build/Bin)의 출력폴더를 다르게 해준다.
+// (실행파일은 남기고 싶을때 Mid만 삭제하면 됨)
+// 
+// 단순히 헤더만 첨가한다고 해당하는 내용(cpp)을 사용할 수는 없다.
+// lib의 경우 pragma comment로 cpp의 내용을 사용 가능하게 만들어야 한다.
+// 근데 dll은 다르다.
+//
+//
 
