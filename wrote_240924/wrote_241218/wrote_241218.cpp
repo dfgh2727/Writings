d6792c1af011da75b1af3211b56b7c747ddc7a17
dfgh2727 <dfgh2727@naver.com>
2024-12-18 11:58:14 +0900

    이수환: CreateBackBuffer()

--- wrote_240924/wrote_241218/wrote_241218.cpp
+++ wrote_240924/wrote_241218/wrote_241218.cpp
@@ -19,7 +19,6 @@
 // D3D_DRIVER_TYPE_UNKNOWN 유저가 직접 그래픽 카드를 선택한다.
 // D3D_DRIVER_TYPE_SOFTWARE 그래픽 카드를 사용하지 않는다. 연산을 CPU에게 맡긴다.
 // 
-// 
 // ****************************************************
 // 
 // int iFlag = 0; 특별한 옵션 없이 사용
@@ -32,5 +31,44 @@
 // 
 // D3D11_SDK_VERSION  현재 다렉의 sdk 버전
 // 
+// CoInitializeEx()
+// 다렉은 쓰레드 안정성을 보장하지 않는다.
 // 
+// GraphicDevice의 Release()는 main함수가 끝난 다음에 지워지는데 
+// leak 확인은 main 함수까지이므로..
+// 명시적으로 EngineEnd()에서 Device와 Context를 지워주겠다.
+
+// CreateBackBuffer()
+//
+// BackBuffer...크기는 자유자재지만 윈도우창 크기로 만드는게 일반적이다.
 // 
+// 다렉에서 BackBuffer를 SwapChain이라 부른다.
+// 백버퍼의 갱신 속력 = 렌더링 속력
+// 다렉은 여러개의 백버퍼를 이용해 일부는 그리고 일부는 지우고 다시 그릴 준비를 한다.
+// 
+// BufferCount = 사용할 백버퍼 수
+// OutputWindow = 윈도우 핸들
+// Windowed = true일시 창화면, false면 전체화면
+// 9전체화면일시 그래픽 카드의 처리 속력이 증가한다. 게임화면에만 집중할 수 있으니.)
+// 
+// ScInfo.BufferDesc.RefreshRate.Dominator = 1
+// ScInfo.BufferDesc.RefreshRate.Numerator = 60 주사율, 모니터에 얼마나 빠르게 갱신할 것인가.
+// 
+// ScInfo.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM 
+// 백버퍼의 색상범위                      RGBA 8비트씩, 32비트                  
+// 
+// ScanlineOrdering 
+// 화면의 픽셀이 갱신되는 순서를 어떻게 할 것인가
+// 위에서부터? 아래에서부터?
+// 
+// ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED 구분 않고 가장 빠른거로 선택한다.
+// 
+// ScInfo.BufferDesc.Scaling
+// 화면 해상도에 맞춰서 이미지를 어떻게 늘릴것인가
+// 
+// ScInfo.BufferUsage = 용도 
+// 다렉... 이미지를 바이트 덩어리로 생각하고 어떻게 사용할 지 결정한다.
+//         
+// ScInfo.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT;
+//                      여기에 그릴 수 있고               쉐이더에서 데이터로도 사용할 수 있다 
+//
