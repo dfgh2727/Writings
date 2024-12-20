7838d3e5734397607b3df403d3d7e2b7ca8cff53
dfgh2727 <dfgh2727@naver.com>
2024-12-20 17:28:34 +0900

    이수환: Texture

--- wrote_240924/wrote_241220/wrote_241220.cpp
+++ wrote_240924/wrote_241220/wrote_241220.cpp
@@ -59,8 +59,37 @@
 // {
 //     Renderer는 Camera로부터 view와 projection을 받아 계산한다.
 // }
+
+// 상수버퍼
 // 
-// CPU에서 계산한 값을 쉐이더에게 넘기는 방법...상수버퍼
+// CPU에서 값을 쉐이더에 전달하기 위해서 쓴다.
+// 크기 제한이 있다. 
+//
+// cbuffer FTransform : register(b0)  - 슬롯의 위치
 // 
+// 쉐이더마다 버퍼가 따로 정해져있다.
 // 
+// void URenderer::ShaderResSetting()
+// {
+//     D3D11_MAPPED_SUBRESOURCE =  
+//      
+//     어떤 데이터를 사용하는 렌더링을 잠깐 정지시킨다.
+//     그래픽 카드를 멈춰서 그래픽 카드의 있는 상수버퍼를 수정한다.
+//     UEngineCore::Device.GetContext()->Map(..., &Data);
 // 
+//     그리고 다 쓴 다음에는 반드시 Unmap 해야한다.
+// 
+// }
+//
+
+// 텍스처
+// 쉐이더의 리소스를 만들려면 쉐이더 리소스 뷰(=셋팅할 권한)가 필요하다. 
+// 
+// DirectX::CreateShaderResourceView()
+// 내부에서 동적할당이 일어나고 있다.
+// 만드는 과정이므로 Device를 이용해야 한다.
+// 
+// TexMetadata 몇비트인가?
+// 
+// ...여차저차 이미지 띄웠다.
+//
