ee99ad924cafd64c6c4c527c768e13c6a598a41d
dfgh2727 <dfgh2727@naver.com>
2025-01-24 10:44:22 +0900

    이수환: auto

--- wrote_240924/wrote_250124/wrote_250124.cpp
+++ wrote_240924/wrote_250124/wrote_250124.cpp
@@ -3,12 +3,20 @@
 
 #include <iostream>
 
+// 110_auto
 // 
+// int Function()
+// {
+//	  return 10;
+// }
 // 
+// void main
+// {
+//    auto Test = Function();
+// }
+// 컴파일 했을 때 자동으로 auto를 수정한다.
+// 결과: int Test = Function();
 // 
-// 
-// 
-// 
-// 
-// 
+// 명시적이지 않아서 선생님은 싫어하는 기능
+// 그냥 변수 이름에 마우스를 대보면 자료형을 보는 용도로...
 // 
