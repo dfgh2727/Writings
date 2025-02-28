1c36b696e4a70fee3e96871bcae614bd45767544
dfgh2727 <dfgh2727@naver.com>
2025-02-28 10:57:42 +0900

    이수환: Item (Component) 장착하기

--- wrote_240924/wrote_250228/wrote_250228.cpp
+++ wrote_240924/wrote_250228/wrote_250228.cpp
@@ -4,9 +4,19 @@
 #include <iostream>
 
 
+// ***주의***
+// struct는 포인터형으로 블루프린트에서 사용할 수 없다.
+// 그렇다고 값형으로 넣기엔 크기가 부담스럽고 null을 확인할 수 없다...
+// 그래서 선생님은 C++을 선호한다.
 // 
+// UStaticMeshComponent* Component = Actor->GetComponentByClass<UStaticMeshComponent>();
+// GetComponentByClass() 내부에 component를 찾기 위해(Find()) for문이 돌고 있다.
+// 그러나 Component 수가 적으면 부담스럽지 않으므로 사용할만 하다.
+// 경우에 따라 적절한 방법이 된다.
+// 엔진 내부에 대한 이해를 바탕으로 코딩을 하자.
 // 
-// 
+// 위 방법 말고 새로 actor를 만드는 방식을 살펴보자.
+// 아이템용 C++클래스를 만든다.
 // 
 
 // 
